cc7eecf60c33150b3f971e3aa8c549de71eaaaaf
Маргарита Кокорева <92210873+Margaret203720@users.noreply.github.com>
2024-05-28 16:03:26 +0300

    Delete 4_11 git directory

--- 4_11 git/src/App1.js
+++
@@ -1,9 +0,0 @@
-import "./styles.css";
-
-export default function App1() {
-  return (
-    <div className="App1">
-      <h1>Путешествие</h1>
-    </div>
-  );
-}
--- 4_11 git/src/App2.js
+++
@@ -1,9 +0,0 @@
-import "./styles.css";
-
-export default function App2() {
-  return (
-    <div className="App2">
-      <h2>Лучшие авиалинии для комфортного путешествия эконом-классом</h2>
-    </div>
-  );
-}
